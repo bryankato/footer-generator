bea328b7fdc55c75527011108bd99bab0a633140
Bryan Kato <bkato@google.com>
2018-09-27 10:17:00 -0700

    initial commit

---
+++ code.js
@@ -0,0 +1,11 @@
+// get sheet
+// get tabs
+// create tab list for dropdown
+// get program name headers
+// get languages for dropdown
+// get footer content
+// show footer content
+
+function myFunction() {
+
+}
